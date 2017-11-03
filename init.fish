da8cb16e74a74e187254738be542627fc896ca12
Jade Meskill <jade.meskill@gmail.com>
2017-11-02 20:04:51 -0700

    fix and

--- init.fish
+++ init.fish
@@ -1,5 +1,5 @@
-set -q GOBIN; or set -l golang_bin_path "$GOBIN"
-set -q GOPATH; or set -l golang_bin_path "$GOPATH/bin"
+set -q GOBIN; and set -l golang_bin_path "$GOBIN"
+set -q GOPATH; and set -l golang_bin_path "$GOPATH/bin"
 set -q golang_bin_path; or set -l golang_bin_path "$HOME/go/bin"
 
 contains -- $golang_bin_path $PATH
