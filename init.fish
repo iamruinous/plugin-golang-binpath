5c59abbdfcb09be7b4a9cfc387dfaafc8df2d54d
Jade Meskill <jade.meskill@gmail.com>
2017-11-02 19:31:41 -0700

    add quotes

--- init.fish
+++ init.fish
@@ -1,7 +1,7 @@
 function init -a path --on-event init_golang-binpath
-  set -q GOBIN; or set -l golang_bin_path $GOBIN
-  set -q GOPATH; or set -l golang_bin_path $GOPATH/bin
-  set -q golang_bin_path; or set -l golang_bin_path $HOME/go/bin
+  set -q GOBIN; or set -l golang_bin_path "$GOBIN"
+  set -q GOPATH; or set -l golang_bin_path "$GOPATH/bin"
+  set -q golang_bin_path; or set -l golang_bin_path "$HOME/go/bin"
   contains -- $golang_bin_path $PATH
     or set -gx PATH $golang_bin_path $PATH
 end
